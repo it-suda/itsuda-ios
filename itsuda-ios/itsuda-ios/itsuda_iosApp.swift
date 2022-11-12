aa6bb7f4310c947f5e6822536efe63c4160c24de
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-11-12 21:12:22 +0900

    create project

---
+++ itsuda-ios/itsuda-ios/itsuda_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  itsuda_iosApp.swift
+//  itsuda-ios
+//
+//  Created by woo0 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct itsuda_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
